eb2153eb288e39dba3b8d3a18ac38a157b620bae
jongs <jongsys21@naver.com>
2022-08-16 22:38:32 +0900

    Cache Share

---
+++ CSharpSample/ConCurrencyInCSharp/00_Thread/00_CacheSharing.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConCurrencyInCSharp._00_Thread
+{
+    internal class _00_CacheSharing
+    {
+        // 여길 바꿔서 테스트
+        const int offset = 1;
+        const int gap = 0;
+        public static int[] sharedData = new int[4 * offset + gap * offset];
+        public static long DoFalseSharingTest(int threadsCount, int size =
+            100_000_000)
+        {
+            Thread[] workers = new Thread[threadsCount];
+            for (int i = 0; i < threadsCount; ++i)
+            {
+                workers[i] = new Thread(new ParameterizedThreadStart(idx =>
+                {
+                    int index = (int)idx + gap;
+                    for (int j = 0; j < size; ++j)
+                    {
+                        sharedData[index * offset] = sharedData[index * offset] +
+                                                     1;
+                    }
+                }));
+            }
+            for (int i = 0; i < threadsCount; ++i)
+                workers[i].Start(i);
+            for (int i = 0; i < threadsCount; ++i)
+                workers[i].Join();
+            return 0;
+        }
+
+        static void Main()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            DoFalseSharingTest(4);
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds + " ms");
+        }
+    }
+}
