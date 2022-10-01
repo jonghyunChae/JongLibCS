9439f862df16d4482313f01dc312da90751b29c1
jongs <jongsys21@naver.com>
2022-10-01 20:48:34 +0900

    TaskCompleteSource

---
+++ CSharpSample/ConCurrencyInCSharp/01_TPL_Basic/07_TaskCompleteSource.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConCurrencyInCSharp._01_TPL_Basic
+{
+    internal class _07_TaskCompleteSource
+    {
+        static async Task WaitJob(Task task)
+        {
+            await Task.Yield();
+            Console.WriteLine("[WaitJob] Start");
+            try
+            {
+                await task;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[WaitJob] Exception Occur = " + e);
+            }
+
+            Console.WriteLine("[WaitJob] await End");
+            Thread.Sleep(1000);
+            Console.WriteLine("[WaitJob] End");
+        }
+
+        static async Task TaskCompleteTest()
+        {
+            tcs = new TaskCompletionSource();
+
+            var t2 = Task.Run(() => WaitJob(tcs.Task));
+            Console.WriteLine("[TaskCompleteTest] Start");
+            Thread.Sleep(3000);
+            Console.WriteLine("[TaskCompleteTest] Sleep End " + t2.IsCompleted);
+            tcs.TrySetResult();
+            Console.WriteLine("[TaskCompleteTest] TrySetResult");
+            await t2;
+            Console.WriteLine("[TaskCompleteTest] t2 End");
+        }
+
+        static async Task TimeoutTest()
+        {
+            tcs = new TaskCompletionSource();
+            var cancel = new CancellationTokenSource(2000);
+            using var canceled = cancel.Token.Register(() =>
+            {
+                Console.WriteLine("[TimeoutTest] Canceled!");
+                if (!tcs.Task.IsCompleted)
+                {
+                    tcs.TrySetCanceled(cancel.Token);
+                }
+            }, useSynchronizationContext: false);
+
+            var t2 = Task.Run(() => WaitJob(tcs.Task));
+            Console.WriteLine("[TimeoutTest] Start");
+            Thread.Sleep(5000);
+            Console.WriteLine("[TimeoutTest] Sleep End " + t2.IsCompleted);
+            await t2;
+            Console.WriteLine("[TimeoutTest] t2 End");
+        }
+
+        static TaskCompletionSource tcs = null!;
+
+        static async Task Main()
+        {
+            await TaskCompleteTest();
+            Console.WriteLine();
+            await TimeoutTest();
+        }
+    }
+}
