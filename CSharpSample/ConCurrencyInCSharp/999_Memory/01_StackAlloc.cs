a3556d9c5dab698c690f8f0eb19995e2055fe709
jongs <jongsys21@naver.com>
2024-01-14 17:15:18 +0900

    span stackalloc

---
+++ CSharpSample/ConCurrencyInCSharp/999_Memory/01_StackAlloc.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Concurrent;
+
+namespace ConCurrencyInCSharp._04_TaskScheduler;
+
+internal class _01_StackAlloc
+{
+    static void Main(string[] args)
+    {
+        Span<int> alist = stackalloc int[]{ 1, 2, 3, 4};
+        Do(alist);
+    }
+
+    static void Do(ReadOnlySpan<int> list)
+    {
+        // do something
+    }
+}
