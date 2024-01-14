35a31f1503acc3153d3bc09feb443d6f4d644092
jongs <jongsys21@naver.com>
2024-01-14 17:19:36 +0900

    array pool

---
+++ CSharpSample/ConCurrencyInCSharp/999_Memory/02_ArrayPool.cs
@@ -0,0 +1,19 @@
+﻿using System.Buffers;
+using System.Collections.Concurrent;
+
+namespace ConCurrencyInCSharp._04_TaskScheduler;
+
+internal class _02_ArrayPool
+{
+    static void Main(string[] args)
+    {
+        var pool = ArrayPool<int>.Shared;
+        Span<int> alist = pool.Rent(4);
+        Do(alist);
+    }
+
+    static void Do(ReadOnlySpan<int> list)
+    {
+        // do something
+    }
+}
